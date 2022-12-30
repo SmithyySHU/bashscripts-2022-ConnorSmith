5fece42bb3660d9cefdf98590efd1f556e8a29c3
SmithyySHU <c0033267@hallam.shu.ac.uk>
2022-12-30 16:25:50 +0000

    Email.sh added update to devprogram and testing

--- devprogram.sh
+++ devprogram.sh
@@ -20,7 +20,8 @@ readwrite(){
 }
 
 email(){
-    break
+    
+    mail -s 'Weekly Stock Update Request' c0033267@hallam.shu.ac.uk <<< 'Please reply to this email with an update of the weekly stocktakings'
 }
 
 cat menu.txt
---
+++ email.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mail -s 'Weekly Stock Update Request' c0033267@hallam.shu.ac.uk <<< 'Please reply to this email with an update of the weekly stocktakings"
